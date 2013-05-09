9b46580337081835b76d4ff712009dfe81581ce5
Drew Blessing <drew.blessing@buckle.com>
2013-05-08 21:34:15 -0500

    Add time autoload

--- lib/gitlab_cli/command.rb
+++ lib/gitlab_cli/command.rb
@@ -2,6 +2,8 @@ module GitlabCli
   module Command
     autoload :Project,             'gitlab_cli/command/project'
     autoload :Snippet,             'gitlab_cli/command/snippet'
+
+    autoload :Time,                'time'
   end
 end
 
